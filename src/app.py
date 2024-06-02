0e63e3ca05d8648dcb69ac7c26742e49b6757a8b
bramosm <brinhelm@gmail.com>
2024-06-01 21:09:21 -0400

    On branch main  Your branch is ahead of 'origin/main' by 2 commits.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   src/__pycache__/database.cpython-312.pyc
    	modified:   src/app.py
    	modified:   src/database.py
    	modified:   src/templates/index.html

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
-#import database as db
+import database as db
 
 template_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 template_dir = os.path.join(template_dir, 'src', 'templates')
@@ -10,8 +10,28 @@ app = Flask(__name__, template_folder = template_dir)
 #Rutas de la aplicación
 @app.route('/')
 def home():
-   
-    return render_template('index.html')
+    cursor = db.database.cursor()
+    cursor.execute("SELECT * FROM prueba")
+    myresult = cursor.fetchall()
+    #Convertir los datos a diccionario
+    insertObject = []
+    columnNames = [column[0] for column in cursor.description]
+    for record in myresult:
+        insertObject.append(dict(zip(columnNames, record)))
+    cursor.close()
+    return render_template('index.html', data=insertObject)
 
-if __name__ == '__main__':
-    app.run(debug=True, port=4000)
+#Ruta para guardar usuarios en la bdd
+@app.route('/prueba', methods=['POST'])
+def addPrueba():
+    titulo = request.form['Titulo']
+    estudiante = request.form['Estudiante']
+    materia = request.form['Materia']
+
+    if titulo and estudiante and materia:
+        cursor = db.database.cursor()
+        sql = "INSERT INTO prueba (Titulo, Estudiante, Materia) VALUES (%s, %s, %s)"
+        data = (titulo, estudiante, materia)
+        cursor.execute(sql, data)
+        db.database.commit()
+    return redirect(url_for('home'))
